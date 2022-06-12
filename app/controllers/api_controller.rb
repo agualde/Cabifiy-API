6de49f115561942f9db8f6e370a7643967ffb2e8
Andres Gualde <andres@gualde.com>
2022-06-12 12:07:49 +0200

    Fixed queue bug

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,31 +4,12 @@ class ApiController < ApplicationController
     render status: 200
   end
 
-  def update    
-    cars_to_hash
+  def update   
+    reset_data_structures 
 
-    @@queues = [[],[],[],[],[],[]]
+    create_cars
 
-    @@active_trips = []
-
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Available cars:"
-    p @@available_cars
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Active trips:"
-    p @@active_trips
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Queues:"
-    p @@queues
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------" 
+    get_info
 
     render status: 200
   end
@@ -39,26 +20,9 @@ class ApiController < ApplicationController
       people: journey_params["people"]
     }
 
-    if_car_available(@journey)
+    find_car_for_group(@journey)
 
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Available cars:"
-    p @@available_cars
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Active trips:"
-    p @@active_trips
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Queues:"
-    p @@queues
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------" 
+    get_info
 
   end
 
@@ -66,102 +30,22 @@ class ApiController < ApplicationController
   def drop_off
     group_id = params["ID"].to_i
 
-    @@active_trips.each do |trip|
-      if trip.keys == [group_id]
-        @found_car = trip[group_id][:car]
-        @journey = trip[group_id][:journey]
-        @@active_trips.delete_if {|h| h[group_id]}
-      end
-    end
-
-    @@available_cars[@found_car[:available_seats]].delete(@found_car[:id])
-
-    @new_available_seats = @found_car[:available_seats] + @journey[:people]
-
-    @found_car[:available_seats] = @new_available_seats
-
-    @@available_cars[@found_car[:available_seats]][@found_car[:id]] = {
-      id: @found_car[:id],
-      seats: @found_car[:seats],
-      available_seats: @found_car[:available_seats]
-    }
+    generate_drop_off(group_id)
 
     update_car_seats_in_active_rides_hash(@found_car)
 
-    queue_state = false
-       
-      @@queues.each do |queue|
-        if queue.first
-          queue_state = true
-        end
-      end
-      
-      if queue_state
-        @wait_list = []
-        
-        @@queues.each do |queue|
-          if queue.first && queue.first[:people] <= @found_car[:available_seats]
-            @wait_list << queue.first
-          end
-        end
-          
-          wait_times =  @wait_list.collect { |x| x[:time] }
-          
-          wait_times = wait_times.sort
-          
-          longest_time = wait_times.first 
-          
-          @wait_list.each do |group|
-            if group[:time] == longest_time
-              @longest_waiting_group_that_fits_in_car = group
+    if_group_waiting_find_them_car
 
-              @@queues.each do |queue|
-                if queue.first == @longest_waiting_group_that_fits_in_car
-                  queue.shift
-                end
-              end
-            end
-          end
-      
-        if_car_available(@longest_waiting_group_that_fits_in_car)
-      end
-
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Available cars:"
-    p @@available_cars
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Active trips:"
-    p @@active_trips
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------"
-    puts "Queues:"
-    p @@queues
-    puts "---------------------"
-    puts "---------------------"
-    puts "---------------------" 
+    get_info
 
   end
 
   def locate
     car_id = params["ID"].to_i
 
-    @@active_trips.each do |trip|
-      if trip.keys == [car_id]
-        @found_car = trip[car_id][:car]
-      end
-    end
-    
-    car = {
-      id: @found_car[:id],
-      seats: @found_car[:seats]
-    }   
+    find_car_from_group(car_id)
 
-    render json: car
+    render json: @car
   end
 
   def error
@@ -171,16 +55,29 @@ class ApiController < ApplicationController
 
   private
 
-  def journey_params
-    params.permit!["api"]
+  def reset_data_structures
+    @@queues = [[],[],[],[],[],[]]
+    @@active_trips = []
+    @@available_cars = [{},{},{},{},{},{},{}]
   end
 
-  def car_params
-    params.permit!["_json"]
+  def create_cars
+    @cars = car_params
+
+    @cars.each do |car|
+      for i in 1..6
+        if car["seats"] == i
+          @@available_cars[i][car["id"]] = {
+              id: car["id"],
+              seats: car["seats"],
+              available_seats: car["seats"]
+            }
+        end
+      end
+    end
   end
 
-
-  def if_car_available(journey)
+  def find_car_for_group(journey)
     @@riding = false
 
     for i in (journey[:people]..6)
@@ -224,48 +121,13 @@ class ApiController < ApplicationController
 
       journey_queue(@journey)
 
-      puts "QUEUE FORMED"
-      puts "---------------------"
-      puts "---------------------"
-      puts "---------------------"
-      puts "Available cars:"
-      p @@available_cars
-      puts "---------------------"
-      puts "---------------------"
-      puts "---------------------"
-      puts "Active trips:"
-      p @@active_trips
-      puts "---------------------"
-      puts "---------------------"
-      puts "---------------------"
-      puts "Queues:"
-      p @@queues
-      puts "---------------------"
-      puts "---------------------"
-      puts "---------------------" 
+      get_info
 
       render status: 200
     end
   end
 
-  def cars_to_hash
-    
-    @@available_cars = [{},{},{},{},{},{},{}]
 
-    @cars = car_params
-
-    @cars.each do |car|
-      for i in 1..6
-        if car["seats"] == i
-          @@available_cars[i][car["id"]] = {
-              id: car["id"],
-              seats: car["seats"],
-              available_seats: car["seats"]
-            }
-        end
-      end
-    end
-  end
 
   def journey_queue(journey)
     for i in 1..6
@@ -285,5 +147,106 @@ class ApiController < ApplicationController
         active_trip_hash.values[0][:car][:available_seats] = @new_available_seats
       end
     end
+  end
+
+  def generate_drop_off(group_id)
+    @@active_trips.each do |trip|
+      if trip.keys == [group_id]
+        @found_car = trip[group_id][:car]
+        @journey = trip[group_id][:journey]
+        @@active_trips.delete_if {|h| h[group_id]}
+      end
+    end
+
+    @@available_cars[@found_car[:available_seats]].delete(@found_car[:id])
+
+    @new_available_seats = @found_car[:available_seats] + @journey[:people]
+
+    @found_car[:available_seats] = @new_available_seats
+
+    @@available_cars[@found_car[:available_seats]][@found_car[:id]] = {
+      id: @found_car[:id],
+      seats: @found_car[:seats],
+      available_seats: @found_car[:available_seats]
+    }
+  end
+
+  def if_group_waiting_find_them_car
+    queue_state = false
+    @wait_list = []
+       
+    @@queues.each do |queue|
+     if queue.first && queue.first[:people] <= @found_car[:available_seats]
+        queue_state = true
+        @wait_list << queue.first
+      end
+    end
+    
+    if queue_state
+      
+      @@queues.each do |queue|
+        
+        wait_times =  @wait_list.collect { |x| x[:time] }
+        
+        wait_times = wait_times.sort
+        
+        longest_time = wait_times.first 
+        
+        @wait_list.each do |group|
+          if group[:time] == longest_time
+            @longest_waiting_group_that_fits_in_car = group
+
+            @@queues.each do |queue|
+              if queue.first == @longest_waiting_group_that_fits_in_car
+                queue.shift
+              end
+            end
+          end
+        end
+      end
+      find_car_for_group(@longest_waiting_group_that_fits_in_car)
+    end
+  end
+
+  def find_car_from_group(car_id)
+    @@active_trips.each do |trip|
+      if trip.keys == [car_id]
+        @found_car = trip[car_id][:car]
+      end
+    end
+    
+    @car = {
+      id: @found_car[:id],
+      seats: @found_car[:seats]
+    }   
+  end
+
+  def journey_params
+    params.permit!["api"]
+  end
+
+  def car_params
+    params.permit!["_json"]
+  end
+
+  def get_info
+    puts "---------------------"
+    puts "---------------------"
+    puts "---------------------"
+    puts "Available cars:"
+    p @@available_cars
+    puts "---------------------"
+    puts "---------------------"
+    puts "---------------------"
+    puts "Active trips:"
+    p @@active_trips
+    puts "---------------------"
+    puts "---------------------"
+    puts "---------------------"
+    puts "Queues:"
+    p @@queues
+    puts "---------------------"
+    puts "---------------------"
+    puts "---------------------" 
   end
 end
